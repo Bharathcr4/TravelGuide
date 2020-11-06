f01d70834c8303a9507608987c512398d16a7f38
Bharath <crbharath4@gmail.com>
2020-11-06 18:42:55 +0530

    First Mini Project

---
+++ app/src/main/java/com/example/travelguide/User/AllCategories.java
@@ -0,0 +1,30 @@
+package com.example.travelguide.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.example.travelguide.R;
+
+public class AllCategories extends AppCompatActivity {
+ImageView backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_all_categories);
+
+
+        //Hooks
+        backBtn = findViewById(R.id.back_pressed);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllCategories.super.onBackPressed();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/travelguide/User/UserDashboard.java
+++ app/src/main/java/com/example/travelguide/User/UserDashboard.java
@@ -1,16 +1,149 @@
 package com.example.travelguide.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.example.travelguide.Common.LoginSignup.RetailerStartUpScreen;
+import com.example.travelguide.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.travelguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.travelguide.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class UserDashboard extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    static final float END_SCALE = 0.7f;
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
+    ImageView menuIcon;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    LinearLayout contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+        //Hooks
+        featuredRecycler = findViewById(R.id.featured_recycler);
+
+        menuIcon = findViewById(R.id.menu_icon);
+
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        contentView = findViewById(R.id.content);
+
+
+        navigationDrawer();
+
+        featuredRecycler();
+
+    }
+
+///Navigation Drawer Functions
+    private void navigationDrawer() {
+
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+
+        drawerLayout.setScrimColor(getResources().getColor(R.color.grey));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+                                           @Override
+                                           public void onDrawerSlide(View drawerView, float slideOffset) {
+                                               final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                                               final float offsetScale = 1 - diffScaledOffset;
+                                               contentView.setScaleX(offsetScale);
+                                               contentView.setScaleY(offsetScale);
+
+
+                                               final float xOffset = drawerView.getWidth() * slideOffset;
+                                               final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                                               final float xTranslation = xOffset - xOffsetDiff;
+                                               contentView.setTranslationX(xTranslation);
+
+                                           }
+                                       }
+        );
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_all_categories:
+                startActivity(new Intent(getApplicationContext(),AllCategories.class));
+                break;
+        }
+
+
+        return true;
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonald, "Mcdonald's", "Mcdonalds is the best as it has many veg options, food is always tasty even though as anywhere extremely unhealthy."));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.temples, "Famous Temples", "A famous Temple in the city where thousands of people visit"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.bakery_img, "Best Bakery in City", "A famous Temple in the city where you get Cakes,Sweets and other food items"));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
+
+    }
+
+
+
+    public void callRetailerScreens(View view){
+        startActivity(new Intent(getApplicationContext(), RetailerStartUpScreen.class));
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
     }
 }
+
+
