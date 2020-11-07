120e89fb492b5a3cd0248277086a262cec8a9f97
Bharath <crbharath4@gmail.com>
2020-11-07 13:56:57 +0530

    My First Mini Project

--- app/src/main/java/com/example/travelguide/Common/LoginSignup/Login.java
+++ app/src/main/java/com/example/travelguide/Common/LoginSignup/Login.java
@@ -13,6 +13,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_retailer_start_up_screen);
+        setContentView(R.layout.activity_retailer_login);
     }
 }
--- app/src/main/java/com/example/travelguide/Common/LoginSignup/RetailerStartUpScreen.java
+++ app/src/main/java/com/example/travelguide/Common/LoginSignup/RetailerStartUpScreen.java
@@ -8,28 +8,36 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.travelguide.R;
 
 public class RetailerStartUpScreen extends AppCompatActivity {
+    private Button loginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_start_up_screen);
+
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+
+
+            Pair[] pairs = new Pair[1];
+            pairs[0] = new Pair<View, String>(loginBtn, "transition_login");
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RetailerStartUpScreen.this, pairs);
+               startActivity(intent, options.toBundle());
+            }else {
+                startActivity(intent);
+            }
+        });
     }
 
-    public void callLoginScreen(View view) {
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-
-        Pair[] pairs = new Pair[1];
-        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn), "transition_login");
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RetailerStartUpScreen.this, pairs);
-        startActivity(intent, options.toBundle());
-    } else {
-     startActivity(intent);
-} }
 }
